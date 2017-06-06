c0402b67d7a150a45def990d86267af7095aa92b
kavunshiva <kavunshiva@gmail.com>
2017-06-06 13:25:34 -0400

    get it to work

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Keypad extends Component {
+  render() {
+    return(
+      <button onFocus={() => console.log("Good!")} onBlur={() => console.log('Hey! Eyes on me!')} />
+    )
+  }
+}
+
+
 // Code EyesOnMe Component Here
